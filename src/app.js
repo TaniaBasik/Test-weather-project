b4e45ecd47c374f896376bf36649c03952c62e37
TaniaBasik <109100202+TaniaBasik@users.noreply.github.com>
2022-09-19 21:37:11 +0300

    Installed Axios and current temperarure data

---
+++ src/app.js
@@ -0,0 +1,15 @@
+function displayTemp(responce) {
+  console.log(responce.data);
+  let tempElement = (document.querySelector("#temperature").innerHTML =
+    Math.round(responce.data.main.temp));
+  let conditionElement = (document.querySelector("#condition").innerHTML =
+    responce.data.weather[0].description);
+  let cityElement = (document.querySelector("#city").innerHTML =
+    responce.data.name);
+}
+
+let apiKey = "f1064a94adcdf33252dd0ace190896af";
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemp);
