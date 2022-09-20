5d50803db907fd089edbe6d7409a50d2074ada03
TaniaBasik <109100202+TaniaBasik@users.noreply.github.com>
2022-09-20 21:13:41 +0300

    Updated time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let mins = date.getMinutes();
+  if (mins < 10) {
+    mins = `0${mins}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${mins}`;
+}
+
 function displayTemp(responce) {
   console.log(responce.data);
   let tempElement = (document.querySelector("#temperature").innerHTML =
@@ -10,6 +33,9 @@ function displayTemp(responce) {
     responce.data.main.humidity);
   let windEl = (document.querySelector("#wind").innerHTML = Math.round(
     responce.data.wind.speed
+  ));
+  let dateEl = (document.querySelector("#date").innerHTML = formatDate(
+    responce.data.dt * 1000
   ));
 }
 
